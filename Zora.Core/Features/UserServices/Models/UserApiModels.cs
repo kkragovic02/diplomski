806a9b8f9f05facc3564f87bfdbfaf4f77f7f30f
Кристина Краговић <kk210270d@student.etf.bg.ac.rs>
2025-06-15 11:52:55 +0200

    Change UpdateUser to record

--- Zora.Core/Features/UserServices/Models/UserApiModels.cs
+++ Zora.Core/Features/UserServices/Models/UserApiModels.cs
@@ -6,12 +6,4 @@ public record User(long Id, string Name, string Email, Role Role);
 
 public record CreateUser(string Name, string Email, string Password);
 
-public class UpdateUser
-{
-    public string? Name { get; set; }
-
-    public string? Email { get; set; }
-
-    public string? Password { get; set; }
-    public Role? Role { get; set; }
-};
+public record UpdateUser(string? Name, string? Email, string? Password, Role? Role);
